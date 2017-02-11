79d7a78d3ef68ace78cb7efc48024eaae297ba68
dongyeforever <dongyeforever@foxmail.com>
2017-02-11 11:46:50 +0800

    正则demo增加

---
+++ regex/re_str_demo.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+import re
+
+' 字符串中相关操作 '
+
+string = 'imooc videonum = 999'
+
+# sub 替换
+info = re.sub(r'\d+', '1001', string)
+
+print(info)
+
+
+def add1(match):
+    val = match.group()
+    num = int(val) + 1
+    return str(num)
+
+info = re.sub(r'\d+', add1, string)
+print(info)
+
+# split 分割
+string2 = 'imooc:C C++ Java Python'
+print(re.split(r':| ', string2))
