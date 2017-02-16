9bf129e1cf975fa548b7165d9720093f59c38454
dongyeforever <dongyeforever@foxmail.com>
2017-02-16 13:11:57 +0800

    第三方库 PIL

---
+++ third/PIL_demo.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+
+from PIL import Image, ImageFilter
+
+from PIL import ImageDraw, ImageFont
+import random
+
+
+class MyGaussianBlur(ImageFilter.Filter):
+    name = "GaussianBlur"
+
+    def __init__(self, radius=2, bounds=None):
+        self.radius = radius
+        self.bounds = bounds
+
+    def filter(self, image):
+        if self.bounds:
+            clips = image.crop(self.bounds).gaussian_blur(self.radius)
+            image.paste(clips, self.bounds)
+            return image
+        else:
+            return image.gaussian_blur(self.radius)
+
+
+# 打开一个jpg图像文件，注意是当前路径:
+im = Image.open('test.jpg')
+# 应用模糊滤镜:
+im2 = im.filter(MyGaussianBlur(radius=20))
+im2.save('blur.jpg', 'jpeg')
+
+import random
+
+
+# 随机字母:
+def rndChar():
+    return chr(random.randint(65, 90))
+
+
+# 随机颜色1:
+def rndColor():
+    return (random.randint(64, 255), random.randint(64, 255), random.randint(64, 255))
+
+
+# 随机颜色2:
+def rndColor2():
+    return (random.randint(32, 127), random.randint(32, 127), random.randint(32, 127))
+
+
+# 240 x 60:
+width = 60 * 4
+height = 60
+image = Image.new('RGB', (width, height), (255, 255, 255))
+# 创建Font对象:
+font = ImageFont.truetype('/usr/share/fonts/adobe-source-han-sans/SourceHanSansSC-Normal.otf', 36)
+# 创建Draw对象:
+draw = ImageDraw.Draw(image)
+# 填充每个像素:
+for x in range(width):
+    for y in range(height):
+        draw.point((x, y), fill=rndColor())
+# 输出文字:
+for t in range(4):
+    draw.text((60 * t + 10, 10), rndChar(), font=font, fill=rndColor2())
+# 模糊:
+image = image.filter(ImageFilter.BLUR)
+image.save('code.jpg', 'jpeg')
